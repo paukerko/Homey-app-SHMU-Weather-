1de0012fc02a00e158b659cd7a640145a5f98971
paukerko <pauko.martinn@gmail.com>
2026-03-07 22:30:25 +0100

    Add pairing for Mochovce Weather driver

--- drivers/mochovce-weather/driver.js
+++ drivers/mochovce-weather/driver.js
@@ -1,34 +1,22 @@
-'use strict';
+"use strict";
 
-const Homey = require('homey');
+const Homey = require("homey");
 
-module.exports = class MyDriver extends Homey.Driver {
-
-  /**
-   * onInit is called when the driver is initialized.
-   */
+class MochovceWeatherDriver extends Homey.Driver {
   async onInit() {
-    this.log('MyDriver has been initialized');
+    this.log("Mochovce Weather driver initialized");
   }
 
-  /**
-   * onPairListDevices is called when a user is adding a device
-   * and the 'list_devices' view is called.
-   * This should return an array with the data of devices that are available for pairing.
-   */
   async onPairListDevices() {
     return [
-      // Example device data, note that `store` is optional
-      // {
-      //   name: 'My Device',
-      //   data: {
-      //     id: 'my-device',
-      //   },
-      //   store: {
-      //     address: '127.0.0.1',
-      //   },
-      // },
+      {
+        name: "Mochovce Weather",
+        data: {
+          id: "mochovce-weather",
+        },
+      },
     ];
   }
+}
 
-};
+module.exports = MochovceWeatherDriver;
